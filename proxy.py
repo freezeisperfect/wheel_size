a33ceff3973b6baefaa21a4c5e49c4b2e3a7a020
jackiezui <chenfang199531@gmail.com>
2021-03-16 20:18:17 +0800

    update

--- proxy.py
+++ proxy.py
@@ -53,5 +53,5 @@ class Proxy:
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
-            t_driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+            t_driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
         return t_driver
